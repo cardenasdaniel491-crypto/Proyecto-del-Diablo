55cf291d4e9c7d49ce3108481dd292c03d01e22a
Daniel Cardenas <cardenasdaniel491@gmail.com>
2025-10-30 15:17:42 -0500

    Agregue mas formulas

--- formulas.py
+++ formulas.py
@@ -1,12 +1,33 @@
 import math
 
 def d(D,n): #Convertir de meses o años a días
-    return D/n
+    d = D/n
+    return d
+
+def H(c,I): #Costo de mantenimiento
+    H = c*I
+    return H
 
 def h(H,n): #Convertir de meses o años a días
-    return H/n
+    h = H/n
+    return h
 
-def Q(S,d,h): #Formula general de Q
-    T = math.sqrt((2*S*d)/h) #Total
-    return T
+def Q(S,d,h): #Formula de cantidad economica de pedido sin escasez
+    Q = math.sqrt((2*S*d)/h) #Total
+    return Q
 
+def CTU(S,d,h,Q): #Costo total unitario sin escasez
+    CTU = (S*d)/Q + (h*Q)/2
+    return CTU
+
+def t(Q,d): #Tiempo entre pedidos sin escasez
+    t = Q/d
+    return t
+
+def PR(L,d,t): #Periodo de reabastecimiento sin escasez
+    n=L/t
+    if L<t:
+        PR = L*d
+    else:
+        PR = L-(n*t)
+    return PR
