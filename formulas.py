dd5c13d5e1eb84f64b49d6936feac8481237cb89
Daniel Cardenas <cardenasdaniel491@gmail.com>
2025-10-30 16:42:58 -0500

    Agregue 2 formulas

--- formulas.py
+++ formulas.py
@@ -25,9 +25,18 @@ def t(Q,d): #Tiempo entre pedidos sin escasez
     return t
 
 def PR(L,d,t): #Periodo de reabastecimiento sin escasez
-    n=L/t
+    n = L/t
     if L<t:
         PR = L*d
     else:
         PR = L-(n*t)
     return PR
+
+def p(Ca,n):
+    p = Ca/n
+    return p
+
+def Qs(S,d,h,p): #Formula de cantidad economica de pedido con escasez
+    Qs = math.sqrt((2*S*d*(p+h))/(p*h))
+    return Qs
+
