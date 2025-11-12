d3fe8139c1aac5d117e46f4184ae6da9fdbfd9b0
Daniel Cardenas <cardenasdaniel491@gmail.com>
2025-11-12 14:21:58 -0600

    Agregue Q, Sm, ConvA y t1

--- proyecto/src/formulario.py
+++ proyecto/src/formulario.py
@@ -18,6 +18,43 @@ def ConvD (D,n,op): #Conversion de D a d y viceversa
         ConvD = D * n
     return ConvD
 
-def Q (D,S,H): #Cantidad economica de pedido
-    Q = math.sqrt((2 * D * S) / H)
+def ConvA (a,n,op): #Conversion de ca o a a anual y viceversa
+    if op == 1: #De A a a
+        ConvA = A / n
+    elif op == 2: #De a a A
+        ConvA = A * n
+    return ConvA
+
+def Q (D,S,H,a,op): #Cantidad Optima de Pedido
+    if op == 1: #General
+        arriba = 2 * D * S
+        division = arriba / H
+        Q = math.sqrt(division)
+    elif op == 2: #Con escacez
+        arriba = 2 * D * S * (H + a)
+        abajo = H * a
+        division = arriba / abajo
+        Q = math.sqrt(division)
+    elif op == 3: #Con produccion
+        arriba = 2 * D * S
+        abajo = H * (1 - (D / a))
+        division = arriba / abajo
+        Q = math.sqrt(division)
     return Q
+
+def Sm (Q,D,a,S,H,op):
+    if op == 1: #Con escacez
+        arriba = 2 * S * D * a
+        abajo = H * (H + a)
+        division = arriba / abajo
+        Sm = math.sqrt(division)
+    elif op == 2: #Con produccion
+        Sm = Q * (1 - (D / a))
+    return Sm
+
+def t1 (Q,D,a,op): #Tiempo de produccion
+    if op == 1: #Sin produccion
+        t1 = Q / D
+    elif op == 2: #Con produccion
+        t1 = Q / a
+    return t1
